1ea37351ea7c6957dc7678cc069dbbc870466488
Joey Yandle <xoloki@gmail.com>
2025-04-14 01:30:20 -0400

    clean up schnorr::ID to use standard variable names for a schnorr proof
    delete sensitive values in the signer after dkg and signing
    
    clear sensitive data from coordinators
    
    add helper functions to zero out schnorr::ID and PolyCommitment; zero out dkg_public_shares and keep after reset in dkg_ended
    
    add helper functions to check if a schnorr::ID and PolyCommitment has been zeroed out; add test to see if sensitive data has been cleared
    
    add missing tests to frost coordinator
    
    fix comment on PolyCommitment::is_zero
    
    use values fn to iterate over map values

--- src/schnorr.rs
+++ src/schnorr.rs
@@ -1,3 +1,4 @@
+use num_traits::Zero;
 use rand_core::{CryptoRng, RngCore};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
@@ -12,46 +13,56 @@ use crate::{
 
 #[allow(non_snake_case)]
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
-/// ID type which encapsulates the ID and a schnorr proof of ownership of the polynomial
+/// Encapsulatetion of the ID and a zero knowledge proof of ownership of a private key bound to ID
 pub struct ID {
-    /// The ID
+    /// ID
     pub id: Scalar,
-    /// The public schnorr response
-    pub kG: Point,
-    /// The aggregate of the schnorr committed values
-    pub kca: Scalar,
+    /// Commitment to the proof random value
+    pub R: Point,
+    /// Sigma protocol response
+    pub s: Scalar,
 }
 
 #[allow(non_snake_case)]
 impl ID {
-    /// Construct a new schnorr ID which binds the passed `Scalar` `id` and `Scalar` `a`, with a zero-knowledge proof of ownership of `a`
-    pub fn new<RNG: RngCore + CryptoRng>(id: &Scalar, a: &Scalar, rng: &mut RNG) -> Self {
-        let k = Scalar::random(rng);
-        let c = Self::challenge(id, &(&k * &G), &(a * &G));
+    /// Construct a new schnorr ID that proves ownership of private key `x` bound to `id`
+    pub fn new<RNG: RngCore + CryptoRng>(id: &Scalar, x: &Scalar, rng: &mut RNG) -> Self {
+        let r = Scalar::random(rng);
+        let R = r * G;
+        let X = x * G;
+        let c = Self::challenge(id, &R, &X);
+        let s = r + c * x;
 
-        Self {
-            id: *id,
-            kG: &k * G,
-            kca: &k + c * a,
-        }
+        Self { id: *id, R, s }
     }
 
     /// Compute the schnorr challenge
-    pub fn challenge(id: &Scalar, K: &Point, A: &Point) -> Scalar {
+    pub fn challenge(id: &Scalar, R: &Point, X: &Point) -> Scalar {
         let mut hasher = Sha256::new();
         let tag = "WSTS/polynomial-constant";
 
         hasher.update(tag.as_bytes());
         hasher.update(id.to_bytes());
-        hasher.update(K.compress().as_bytes());
-        hasher.update(A.compress().as_bytes());
+        hasher.update(R.compress().as_bytes());
+        hasher.update(X.compress().as_bytes());
 
         hash_to_scalar(&mut hasher)
     }
 
-    /// Verify the proof
-    pub fn verify(&self, A: &Point) -> bool {
-        let c = Self::challenge(&self.id, &self.kG, A);
-        &self.kca * &G == &self.kG + c * A
+    /// Verify the proof against the public key `X`
+    pub fn verify(&self, X: &Point) -> bool {
+        let c = Self::challenge(&self.id, &self.R, X);
+        &self.s * &G == &self.R + c * X
+    }
+
+    /// Zero out the schnorr proof
+    pub fn zero(&mut self) {
+        self.R = Point::new();
+        self.s = Scalar::zero();
+    }
+
+    /// Check if schnorr proof is zeroed out
+    pub fn is_zero(&self) -> bool {
+        self.R == Point::new() && self.s == Scalar::zero()
     }
 }
