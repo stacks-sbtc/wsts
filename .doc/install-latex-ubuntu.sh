c8bfd32888289780d389cc8ab8a68db9f90b536c
Joey Yandle <xoloki@gmail.com>
2025-06-26 20:37:45 -0400

    Remove latex-full and instead install extras and science

--- .doc/install-latex-ubuntu.sh
+++ .doc/install-latex-ubuntu.sh
@@ -1,4 +1,8 @@
 # Update apt
 sudo apt-get update
 # Install LaTeX
-sudo apt-get install texlive-full
+sudo apt-get install texlive
+# Install LaTeX Extra
+sudo apt-get install texlive-latex-extra
+# Install Latex Science 
+sudo apt-get install texlive-science
